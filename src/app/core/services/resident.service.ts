c4a1affe822bfd2954beeb2dfa6e06d77c4334be
RohitPatil-MA <rohit.patil@manageamerica.com>
2026-03-04 14:35:56 +0530

    commit

--- src/app/core/services/resident.service.ts
+++ src/app/core/services/resident.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 import {
   BalanceSummary, Transaction, BillingStatement, Notice, AccountSection
 } from '../models/resident.model';
